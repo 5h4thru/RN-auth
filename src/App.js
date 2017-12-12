fa5db375370e2b84e211780c281e6a1c0081de42
Palagummi <paladurg@amazon.com>
2017-12-12 07:59:33 -0800

    Chapter 'Authentication With Firebase' Checkpoint * Compelte firebase setup and init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    // firebase initialization
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAcDws85CvJKE0CVdAfO1tJ2BhyCc4LUoU',
+      authDomain: 'rn-auth-72785.firebaseapp.com',
+      databaseURL: 'https://rn-auth-72785.firebaseio.com',
+      projectId: 'rn-auth-72785',
+      storageBucket: 'rn-auth-72785.appspot.com',
+      messagingSenderId: '938410903984'
+    });
+  }
+
   render() {
     return (
       <View>
